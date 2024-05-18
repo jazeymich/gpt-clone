a24721dd3107b40482a8db858a6b4ea526a88607
jazeymich <josephkabera42@gmail.com>
2024-05-19 00:39:58 +0300

    Page layout:setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
 
 function App() {
   return (
     <div className="App">
-      .sidebar
-      
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="" className="logo" />{" "}
+            <span className="brand">ChatGpt</span>
+            <button className="midBtn">
+              <img className="addBtn" alt="" />
+              New chat
+            </button>
+            <div className="upperSideBottom">
+              <button className="query">
+                <img src=" " alt="" />
+                What is programming ?
+              </button>
+              <button className="query">
+                <img src=" " alt="" />
+                What is programming ?
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
